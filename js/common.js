4286db5c6bf14f17cf5e088ec42a638ef821e8a2
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-01-14 22:25:59 +0300

    1

--- js/common.js
+++ js/common.js
@@ -1,3 +1,69 @@
 $(document).ready(function () {
   
+  jQuery('.header-top-wrap-center ul li:has(ul)').addClass('has-children');
+
+  jQuery('li.has-children').click(function() {
+    jQuery(this).children('ul').slideDown(500);
+  });
+
+  jQuery('li.has-children').mouseenter(function() {
+    jQuery(this).children('ul').slideDown(500);
+  });
+  
+  jQuery('.has-children').mouseleave(function() {
+    jQuery(this).children('ul').slideUp(500);
+  });
+  
+  if ($(window).width() > 992) {
+    $('.portfolio-wrap').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: false,
+      arrows: true
+    });
+
+  }
+ 
+ 
+  var s = document.createElement('style');
+  document.head.appendChild(s);
+  
+  var inputDiv = document.querySelector('#inputDiv');
+  var w = parseInt(window.getComputedStyle(inputDiv, null).getPropertyValue("width"));
+  /* EL INPUT */
+  var elInput = document.querySelector("input[type='range']");
+  elInput.style.width = w +"px";
+  var inputMin = elInput.getAttribute('min');
+  var inputMax = elInput.getAttribute('max');
+  var k = w/(inputMax - inputMin);
+  
+  /* LA ETIQUETA */
+  var etiqueta = document.querySelector('#etiqueta');
+  var ew = parseInt(window.getComputedStyle(etiqueta, null).getPropertyValue("width"));
+  
+  
+  /* el valor de la etiqueta (el tooltip) */
+  etiqueta.innerHTML = elInput.value;
+  /* calcula la posición inicial de la etiqueta (el tooltip) */
+  etiqueta.style.left =  ((elInput.value * k) - (ew/2))+"px";
+  /* establece el estilo inicial del TRACK */
+  s.textContent ="input[type=range]::-webkit-slider-runnable-track{ background-image:-webkit-linear-gradient(left, #4d4dff "+elInput.value+"%,#d3deff "+elInput.value+"%)}"
+  s.textContent +="input[type=range]::-moz-range-track{ background-image:-moz-linear-gradient(left, #4d4dff "+elInput.value+"%,#d3deff "+elInput.value+"%)}"
+  
+  
+  
+  elInput.addEventListener('input',function(){
+    
+  /* cambia el valor de la etiqueta (el tooltip) */
+  etiqueta.innerHTML =elInput.value;
+  /* cambia la posición de la etiqueta (el tooltip) */
+  etiqueta.style.left =  ((elInput.value * k) - (ew/2))+"px";
+  /* cambia el estilo del TRACK */
+  s.textContent ="input[type=range]::-webkit-slider-runnable-track{ background-image:-webkit-linear-gradient(left, #4d4dff "+elInput.value+"%,#d3deff "+elInput.value+"%)}"
+  s.textContent +="input[type=range]::-moz-range-track{ background-image:-moz-linear-gradient(left, #4d4dff "+elInput.value+"%,#d3deff "+elInput.value+"%)}"
+  
+  }, false);
+
+
 });
